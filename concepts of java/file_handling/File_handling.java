8226964ac0237598fc5aef8acc4b1f8d316b8ffd
Sachin <sachinarora0986@gmail.com>
2022-09-17 20:00:38 +0530

    All usefull name of methods are added in file_handling file w'll implement all methods later.

---
+++ concepts of java/file_handling/File_handling.java
@@ -0,0 +1,41 @@
+/*In file handling we use java.io file to handle input-output operations 
+ the java.io file contains many methods like :-
+ 1) canExecute()
+ 2) canRead()
+ 3) canWrite()
+ 4) compareTo(file_pathname)
+ 5) createNewFile()
+ 6) createTempFile(String prefix, String suffix)
+ 7) Delete()
+ 8) equals(objects obj)
+ 9) exists()
+ 10) getAbsolutePath()
+ 11) list()
+ 12) getFreeSpace()
+ 13) getName()
+ 14) getParent()
+ 15) getParentFile()
+ 16) getPath()
+ 17) setReadonly()
+ 18) isDirectory()
+ 19) isFile()
+ 20) isHidden()
+ 21) length()
+ 22) listFile()
+ 23)mkdir()
+ 24) renameTo(File Dest)
+ 25) setExecutable(Boolean Executable)
+ 26) setReadable(Boolean readable)
+ 27) setReadable(Boolean readablee, owner only)
+ 28) setWriteable(Boolean writeable)
+ 29) toString()
+ 30) toURL()
+*/
+
+// code 
+
+import java.io.*;
+public class File_handling 
+{
+    
+}
