c53568fad455df2f09d65bec186815a98b85e5b4
Sachin <sachinarora0986@gmail.com>
2022-09-26 19:35:13 +0530

    final keyword with the single & multiple variables

---
+++ concepts of java/keywords/Final_Variables.java
@@ -0,0 +1,48 @@
+//code to impliment the final keyword with variable 
+/*when the variable is decleared with the final keyword the the value of that variable 
+can't be changed */
+
+package keywords;
+
+public class Final_Variables 
+{
+     //simple final variable decleration 
+
+    void simple()
+    {
+        final int i;
+        i = 20;
+
+        System.out.println(i);
+    }
+    //decleration of multiple final variables
+    void multiple()
+    {
+        final int a;
+        {
+            a = 5;
+        }
+
+        final String c;
+        {
+
+            c = "SACHIN";
+        }
+        final long d;
+        {
+            d = 827948078;
+        }
+        final double pi = 3.14;
+
+
+        System.out.println("int = " + a + "\n" + "String = " + c + "\n" + "long = " + d + "\n" + "double = " + pi);
+
+    }
+    public static void main(String args[])
+    {
+        Final_Variables Fv = new Final_Variables();
+        //Fv.simple();
+
+        Fv.multiple();
+    }    
+}
